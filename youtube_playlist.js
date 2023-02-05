c1cea940157cf2a75b854323639ad07127d50dbd
Murat Yildirim <murat@lemonbrain.ch>
2023-02-05 21:52:51 +0100

    init

---
+++ youtube_playlist.js
@@ -0,0 +1,455 @@
+
+let playlists = [];
+
+let playlist1 = {
+  name: 'A1-B1 Grammatik Aktiv',
+  videos: [
+    'https://www.youtube.com/embed/DHZSFAJby8o',
+    'https://youtube.com/embed/3X18Qtlwp6I',
+    'https://youtube.com/embed/SKZnHCPYJtk',
+    'https://youtu.be/VmgCF5L7JGM',
+    'https://youtu.be/787i-L1pzs8',
+    'https://youtu.be/QCXxzK5P228',
+    'https://youtu.be/S4qgC7QWjUU',
+    'https://youtu.be/6PYs23S7yRU',
+    'https://youtu.be/oAWEVc1dZd8',
+    'https://youtu.be/-0lrp7J6xpI',
+    'https://youtu.be/VrOfz2rlOJ4',
+    'https://youtu.be/VrOfz2rlOJ4',
+    'https://youtu.be/fLut1O_bWDg',
+    'https://youtu.be/9BUbel1wUlc',
+    'https://youtu.be/LnN4EnX8I_E',
+    'https://youtu.be/MkfIIzqU62E',
+    'https://youtu.be/aSDyWBMs6OM',
+    'https://youtu.be/U7px8sNvaQM',
+    'https://youtu.be/WbAddEgYowc',
+    'https://youtu.be/Urat-ZQlWDw',
+    'https://youtu.be/SMNuAGxZza8',
+    'https://youtu.be/r9orv85MoOU',
+    'https://youtu.be/q1wwLdBY_SU',
+    'https://youtu.be/ULD7fk6GR44',
+    'https://youtu.be/zyB0ZsTt2-k',
+    'https://youtu.be/XlwWj4Fb4Fw',
+    'https://youtu.be/ULyeBmezSkI',
+    'https://youtu.be/hCxLz0WaZxs',
+    'https://youtu.be/MH0HFQrBjWo',
+    'https://youtu.be/68bLUvNk7Xc',
+    'https://youtu.be/t4pb3bKzGaU',
+    'https://youtu.be/Rk5zrj84GMw',
+    'https://youtu.be/kOkmlpPZyr4',
+    'https://youtu.be/XiNReagcP9A',
+    'https://youtu.be/524eEp-kFe4',
+    'https://youtu.be/3DxPl3-zWiM',
+    'https://youtu.be/bAh7qy0Ygng',
+    'https://youtu.be/Chuhx6sEf8k',
+    'https://youtu.be/ygBvGoh8gXY',
+    'https://youtu.be/BgfA1Xgr6NQ',
+    'https://youtu.be/boaF5g7_h50',
+    'https://youtu.be/m4ROPteJO0A',
+    'https://youtu.be/ebaV8B10Vds',
+    'https://youtu.be/-gAXuMpLo14',
+    'https://youtu.be/-8BPPWNJ5gc',
+    'https://youtu.be/peYUhfF3Z7k',
+    'https://youtu.be/pQ03-zbMsLM',
+    'https://youtu.be/GL1RUWXpPq4',
+    'https://youtu.be/7lByLRxM1jY',
+    'https://youtu.be/Z2-aqllkTwQ',
+    'https://youtu.be/SVl-XJCEyjg',
+    'https://youtu.be/NQ6e3o1xNDE',
+    'https://youtu.be/IG6O7WxDAnI',
+    'https://youtu.be/ERf74vnRSlY',
+    'https://youtu.be/xPxjgdzANsM',
+    'https://youtu.be/jBOEaBj1QKM',
+    'https://youtu.be/yLfeWVgqdZc',
+    'https://youtu.be/pN13IZxGaM0',
+    'https://youtu.be/esPiXSAL75g',
+    'https://youtu.be/xsh1yB2n_X4',
+    'https://youtu.be/buqHKZP80-w',
+    'https://youtu.be/p3OfcYg8D74',
+    'https://youtu.be/b_4-RRYIRwY',
+    'https://youtu.be/vi8BUi19bNo',
+    'https://youtu.be/sXVxOtKOy8Y',
+    'https://youtu.be/A4PG2RdcQT0',
+    'https://youtu.be/GfV6GtLGY08',
+    'https://youtu.be/Lx7uFLPWgEc',
+    'https://youtu.be/N4Buqrthxe8',
+    'https://youtu.be/irnsYNwDkA4',
+    'https://youtu.be/x-knO3PTsgQ',
+    'https://youtu.be/7JZ0Z3fTrcc',
+    'https://youtu.be/f5BNCvIJn3Q',
+    'https://youtu.be/ijtEhxFgeNA',
+    'https://youtu.be/UT8Rbw1CYyQ',
+    'https://youtu.be/DIdzLIlvtwo',
+    'https://youtu.be/zJrwxYqZ-O4',
+    'https://youtu.be/eWs33-Laj-M',
+    'https://youtu.be/R9SEKCDsKRY',
+    'https://youtu.be/IFUj4EcOOsA',
+    'https://youtu.be/vIIYte_AW_o',
+    'https://youtu.be/DCKSGIHn7SI',
+    'https://youtu.be/yV5nt4tEi-U',
+    'https://youtu.be/ZzUs6ywuRR8',
+    'https://youtu.be/pZsB34mINwA',
+  ]
+}
+
+let playlist2 = {
+  name: 'Brieftraining B1',
+  videos: [
+    'https://youtu.be/JolTIkocOvA',
+    'https://youtu.be/T_55kQMNAdI',
+    'https://youtu.be/4vWcQiH3vj4',
+    'https://youtu.be/bchHgNz9juQ',
+    'https://youtu.be/Lnma01G8KoE',
+    'https://youtu.be/zUj-UvdLDWc',
+    'https://youtu.be/8osShUqs8_k',
+    'https://youtu.be/6bXbzHjnWnI',
+    'https://youtu.be/PUlUY2viJeo',
+    'https://youtu.be/Q9ulofrg5IE',
+    'https://youtu.be/rwkw3l_Mu00',
+    'https://youtu.be/gEKUY6OJyOw',
+    'https://youtu.be/3gD1Ctp2ugc',
+    'https://youtu.be/X7UPPWTr3GU',
+    'https://youtu.be/60wJzWvBUvc',
+    'https://youtu.be/YhTrESCNLhg',
+  ]
+}
+
+let playlist3 = {
+  name: 'telc - DTZ-Übungsheft',
+  videos: [
+    'https://youtu.be/ZvQKyTbO90M',
+    'https://youtu.be/sDM_FWLIMd8',
+    'https://youtu.be/5av7E8M5PNg',
+    'https://youtu.be/hsXUeKjrpQM',
+    'https://youtu.be/cTiFaWk7taY',
+    'https://youtu.be/_VKGFzIUjPc',
+    'https://youtu.be/ZzOX9qZkF6k',
+    'https://youtu.be/vdb5_uLpNcI',
+    'https://youtu.be/eRg0MX-Iq74',
+    'https://youtu.be/yFpvm2FCa6I',
+    'https://youtu.be/-iRLVveNyWI',
+    'https://youtu.be/fXjxoQf7d-Q',
+    'https://youtu.be/XCl1OGsg1io',
+  ]
+}
+
+let playlist4 = {
+  name: 'Prüfungsvorbereitungen A1',
+  videos: [
+    'https://youtu.be/j3JdCJ7opdw',
+    'https://youtu.be/6WFNU3DNHaQ',
+    'https://youtu.be/D24isayQ4V0',
+    'https://youtu.be/JttyD3EeY4M',
+    'https://youtu.be/pXpC74EU_m8',
+  ]
+}
+let playlist5 = {
+  name: 'Prüfungsvorbereitungen A2',
+  videos: [
+    'https://youtu.be/kQliZwjPSfY',
+    'https://youtu.be/tadYWoFnmHg',
+    'https://youtu.be/X828JD9nwQc',
+    'https://youtu.be/1dVbP5cgzcA',
+    'https://youtu.be/RmfFROCH0Z0',
+  ]
+}
+let playlist6 = {
+  name: 'Prüfungsvorbereitungen B1/DTZ',
+  videos: [
+    'https://youtu.be/fjMJHuMOkV8',
+    'https://youtu.be/NMmKJq3oPgo',
+    'https://youtu.be/zE1qjBDlbRU',
+    'https://youtu.be/OpfJUVHW8lQ',
+    'https://youtu.be/6nP0iy80OEE',
+    'https://youtu.be/4BfeOZf798k',
+    'https://youtu.be/anMdXk2gb7s',
+    'https://youtu.be/Pz3z5a1FPHE',
+    'https://youtu.be/uBTu9AMXTJQ',
+    'https://youtu.be/Dl7m_-G16j0',
+    'https://youtu.be/Rc48_pNxTxc',
+    'https://youtu.be/n2xJVpQlcM4',
+  ]
+}
+let playlist7 = {
+  name: 'A2B1 Lektüre - Großstadtliebe',
+  videos: [
+    'https://youtu.be/iiqmPG8zlLo',
+    'https://youtu.be/3KEvRKsYWsA',
+    'https://youtu.be/_Dwt0hwBY9I',
+    'https://youtu.be/ZnoMsmTqIqY',
+    'https://youtu.be/7E1pDoaKORk',
+    'https://youtu.be/mOr3LGnoz3A',
+    'https://youtu.be/DUE5mQwxPQM',
+    'https://youtu.be/0TSg2IYPvKA',
+    'https://youtu.be/m-QNtClLmqQ',
+    'https://youtu.be/8qLBqoaPFyw',
+    'https://youtu.be/n57Ht3SxG5M',
+    'https://youtu.be/vqjJ89-9M8M',
+  ]
+}
+let playlist8 = {
+  name: 'A2B1 Lektüre - Die Entscheidung',
+  videos: [
+    'https://youtu.be/q5Io13EtNnc',
+    'https://youtu.be/ONlcABAZlNg',
+    'https://youtu.be/42wrM24yRUg',
+    'https://youtu.be/bvtAcZ79UiU',
+    'https://youtu.be/DEiLepSYDTc',
+    'https://youtu.be/v6HEJo1xNbE',
+    'https://youtu.be/kOyJinivXOU',
+    'https://youtu.be/nIro7TGyth4',
+    'https://youtu.be/xmbE9duJjkM',
+    'https://youtu.be/MBOCkopYcqw',
+    'https://youtu.be/0kI9qswwqM4',
+    'https://youtu.be/lUPofc0kHUc',
+  ]
+}
+let playlist9 = {
+  name: 'DW - Top Thema',
+  videos: [
+    'https://youtu.be/01kxiUqKhwY',
+    'https://youtu.be/QzCDnyEZIK8',
+    'https://youtu.be/JSsoMba-sLM',
+    'https://youtu.be/KhRUi7SIADU',
+    'https://youtu.be/Gj82Yc7INH0',
+    'https://youtu.be/H7G7M0cBoGk',
+    'https://youtu.be/eCEspIPVBf0',
+    'https://youtu.be/8gj8L9wMxxc',
+    'https://youtu.be/l24zG3K-SYY',
+    'https://youtu.be/VMiPy6bN7Iw',
+    'https://youtu.be/v15iN9bb4F8',
+    'https://youtu.be/yvRdnactJUI',
+    'https://youtu.be/cLncPwFA1Dc',
+    'https://youtu.be/dY3vhMVYIZQ',
+    'https://youtu.be/UI5a5GUoCk4',
+    'https://youtu.be/p-MJqpI8O-g',
+    'https://youtu.be/v6W0hg2E488',
+    'https://youtu.be/DeCy4UdrHEg',
+    'https://youtu.be/GZHDXrxBNgo',
+    'https://youtu.be/R41lmqFGYvY',
+    'https://youtu.be/H8UzojKfT30',
+    'https://youtu.be/ISe3A7gVY2Q',
+    'https://youtu.be/ITJ4BjFe4N0',
+  ]
+}
+let playlist10 = {
+  name: 'B1+ Grammatik Aktiv',
+  videos: [
+    'https://youtu.be/8ArOj0atcfc',
+    'https://youtu.be/P7quF5wNkbw',
+    'https://youtu.be/mWSdZg257bE',
+    'https://youtu.be/YqGradpoDdc',
+    'https://youtu.be/n90UOUCkSNM',
+    'https://youtu.be/DhhP2yZSbR8',
+    'https://youtu.be/0TInYFo6hdE',
+    'https://youtu.be/ylVGBys99Ro',
+    'https://youtu.be/cWH_F71IYVg',
+    'https://youtu.be/Lrdi-X-Z3lk',
+    'https://youtu.be/293DAolICBE',
+    'https://youtu.be/1RTJwg-J6Pk',
+    'https://youtu.be/IHQyoYfSt2w',
+    'https://youtu.be/0x2B37lKIFU',
+    'https://youtu.be/m1rRN5-KSiM',
+  ]
+}
+let playlist11 = {
+  name: 'B2-C1',
+  videos: [
+    'https://youtu.be/kj3nKHZOloU',
+    'https://youtu.be/bPCNX0TamfU',
+    'https://youtu.be/GiOK4WGxxl0',
+    'https://youtu.be/1-fvDD5YH5Y',
+    'https://youtu.be/CTDMHCyhMeM',
+    'https://youtu.be/BXTRRTzn4eQ',
+    'https://youtu.be/QIVU20utHdc',
+    'https://youtu.be/d0dWwoKymmE',
+    'https://youtu.be/dqvPdp9wJ0c',
+    'https://youtu.be/TNHL2QHMXmg',
+    'https://youtu.be/1Tv5cvxv-B0',
+    'https://youtu.be/4VJwO-ZSdAs',
+    'https://youtu.be/CmcqXlaJwak',
+    'https://youtu.be/IkaZMg24Lr0',
+    'https://youtu.be/NULRJx4vXUk',
+    'https://youtu.be/4fMzw2GOsc0',
+    'https://youtu.be/MeTl-ENRa-k',
+    'https://youtu.be/rCkZqu_G7wY',
+    'https://youtu.be/pOEzVN6SsUo',
+    'https://youtu.be/2gBMaKpy224',
+    'https://youtu.be/oSk_wFd7V2o',
+    'https://youtu.be/GDynh04jL4E',
+    'https://youtu.be/l9RH8iamVpo',
+    'https://youtu.be/8ugCmStlqN4',
+    'https://youtu.be/fbagftdSY-g',
+    'https://youtu.be/REM-JW76xU0',
+    'https://youtu.be/e2L2EHia9Xw',
+    'https://youtu.be/QReRvB62HBo',
+    'https://youtu.be/nptDrWmQReM',
+    'https://youtu.be/k4HnCQCSpg0',
+    'https://youtu.be/CRM8YR-sdfo',
+    'https://youtu.be/9Hv1haEgobo',
+    'https://youtu.be/jQ3wH4wQPp8',
+    'https://youtu.be/1fHwQWiq2DI',
+    'https://youtu.be/IQtsJ4CeW8w',
+    'https://youtu.be/_1aIkB0CFhs',
+    'https://youtu.be/T_AQti4P_0c',
+    'https://youtu.be/s89D0aDM1ok',
+    'https://youtu.be/A_khvlaZTOY',
+    'https://youtu.be/2IV5OjrsncI',
+    'https://youtu.be/I8fI4BasRCw',
+    'https://youtu.be/zbRynHPnUSM',
+    'https://youtu.be/NeLKCoyBOWU',
+    'https://youtu.be/Wn1duDPQvk0',
+    'https://youtu.be/fw_PJdG33bo',
+    'https://youtu.be/rArcIT-g-HE',
+    'https://youtu.be/Kpiyz1Mi9VM',
+    'https://youtu.be/ouFSF7idDC0',
+    'https://youtu.be/Ko1uVasxEl8',
+    'https://youtu.be/lzppNxx2rzk',
+    'https://youtu.be/3xcaisy1GXU',
+    'https://youtu.be/DTEci7KG7-E',
+    'https://youtu.be/dJX0veSFUMA',
+    'https://youtu.be/tBtO3GneshU',
+    'https://youtu.be/CmMTbLnjywc',
+    'https://youtu.be/0cfcOJLPBII',
+    'https://youtu.be/JLY9yu1cdpk',
+    'https://youtu.be/IXVC1ijSxFA',
+    'https://youtu.be/bsPOs66Wfdk',
+    'https://youtu.be/hBupJP6BD4w',
+    'https://youtu.be/3leBeIQcAGU',
+    'https://youtu.be/YteBaUadzGo',
+    'https://youtu.be/BYp4cPAgIZM',
+    'https://youtu.be/H2vG5PN9Vyw',
+    'https://youtu.be/2yd5CJQewwU',
+    'https://youtu.be/DtmXGLMXFXg',
+    'https://youtu.be/fJlJ9eoOh0I',
+    'https://youtu.be/apa1NxC_8Sg',
+    'https://youtu.be/NSO4frGZHL0',
+    'https://youtu.be/6014a_dv4Nc',
+    'https://youtu.be/lBeV48Jk56c',
+    'https://youtu.be/NklZ3dzWrlU',
+    'https://youtu.be/Oj2S_14_6vo',
+    'https://youtu.be/VehXryXxN44',
+    'https://youtu.be/PTUw02RtuAA',
+    'https://youtu.be/elmoKxklzws',
+    'https://youtu.be/aoZfTj2BrOA',
+    'https://youtu.be/lpKTGPbhCPE',
+    'https://youtu.be/42RlqBdmlJk',
+    'https://youtu.be/inAo66LPECY',
+    'https://youtu.be/3lxKVtN0yWY',
+    'https://youtu.be/saQayDCOFDw',
+    'https://youtu.be/wR3VgNbbOSg',
+    'https://youtu.be/MAZ6jLEseFk',
+    'https://youtu.be/ey0Qt2-15Vc',
+    'https://youtu.be/Yw81ZGLRERE',
+    'https://youtu.be/rbJ0oHMvse4',
+    'https://youtu.be/_VMAkZY_vZk',
+    'https://youtu.be/Mnd17FBUrs8',
+    'https://youtu.be/n4BJAEtafB4',
+    'https://youtu.be/zM3wGU9F6wI',
+    'https://youtu.be/nt98uQgmVgg',
+    'https://youtu.be/yEhN09nl-Ss',
+  ]
+}
+let playlist12 = {
+  name: 'B2 - Ek ders & Prüfungsvorbereitung',
+  videos: [
+    'https://youtu.be/gv-nQB8Me_I',
+    'https://youtu.be/cc8b1-3GHCQ',
+    'https://youtu.be/KdBLLlGfIV4',
+    'https://youtu.be/hDrNNQng4VY',
+    'https://youtu.be/4UXUwXz8UT4',
+    'https://youtu.be/w75IXUQXO-Q',
+  ]
+}
+
+playlists.push(playlist1);
+playlists.push(playlist2);
+playlists.push(playlist3);
+playlists.push(playlist4);
+playlists.push(playlist5);
+playlists.push(playlist6);
+playlists.push(playlist7);
+playlists.push(playlist8);
+playlists.push(playlist9);
+playlists.push(playlist10);
+playlists.push(playlist11);
+playlists.push(playlist12);
+
+
+const wrapperEl = document.getElementById('playlists')
+
+playlists.forEach((playlist) => {
+  // create playlist container
+  let playlistContainerEl = document.createElement('section');  
+
+  // put title div into playlist container
+  let titleEl = document.createElement('div');
+  titleEl.classList.add('title');
+
+  let title = document.createElement('h2');
+  title.textContent = playlist.name;
+
+  let titleIconEl = document.createElement('i');
+  titleIconEl.classList.add('fa-solid');
+  titleIconEl.classList.add('fa-caret-down');
+
+  titleEl.appendChild(title);
+  titleEl.appendChild(titleIconEl);
+
+  // create unordered list element for wrapping videos
+  let listContainerEl = document.createElement('ul');
+  playlist.videos.forEach((videoUrl, index)=> {
+    // create list element for a video item
+    let listEl = document.createElement('li');
+
+    // create title inside this
+    let listElTitle = document.createElement('h3');
+    listElTitle.textContent = `Lektion ${++index}`;
+
+    // create icon element for a video item
+    let iconEl = document.createElement('i');
+    iconEl.classList.add('fa-solid');
+    iconEl.classList.add('fa-caret-down');
+
+    // create iframe element for video link
+    let iframeEl = document.createElement('iframe');
+    iframeEl.src= videoUrl;
+    iframeEl.allow = 'fullscreen';
+
+    // append title element to list element
+    listEl.appendChild(listElTitle);
+    // append icon element to list element
+    listEl.appendChild(iconEl);
+    // append iframe element to list element
+    listEl.appendChild(iframeEl);
+
+    // add event listener for toglling showing videos
+    listEl.addEventListener('click', () => {
+      listEl.classList.toggle('showVideo');
+      listElTitle.classList.toggle('active');
+      iconEl.classList.toggle('active');
+      iconEl.classList.toggle('fa-caret-down')
+      iconEl.classList.toggle('fa-caret-right')
+    })
+
+    // append list element to unordered list element
+    listContainerEl.appendChild(listEl);
+  })
+  
+  // append title element
+  playlistContainerEl.appendChild(titleEl);
+  // append unordered list element to playlist container
+  playlistContainerEl.appendChild(listContainerEl);
+  
+  // on click of title of sections video playlists should toggle showing
+  titleEl.addEventListener('click', () => {
+    listContainerEl.classList.toggle('showPlaylist')
+    titleIconEl.classList.toggle('fa-caret-down');
+    titleIconEl.classList.toggle('fa-caret-right');
+    title.classList.toggle('active');
+    titleIconEl.classList.toggle('active');
+  })
+  
+  // append playlist container
+  wrapperEl.appendChild(playlistContainerEl);
+})
